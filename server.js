cb629744e0586e2e9c750008bd359ded72b291c5
Danny Mackey <dmack928@gmail.com>
2019-01-02 11:07:15 -0500

    ejs

--- server.js
+++ server.js
@@ -13,25 +13,80 @@ const port    = 3000;
 
 
 
+// create a helper function that render server informatin in the browser
+
+function display(req, res) {
+  res.json({
+    'req.params': req.params,
+    'req.url'   : req.url,
+    'req.query' : req.query,
+    'req.body'  : req.body
+
+  })
+};
 
 
+//create home route
+app.get('/aboutme', (req,res) => {
 
-//create  routes
+  app.get('/aboutme/description', (req, res) => {
+    res.render('aboutme.ejs', {
+      question: 'Tell me a little bit about yourself?',
+      answer: 'description...'
+    });
+  });
 
-app.get('/', (req,res) => {
-  res.send ('Test /')
-});
+  app.get('/aboutme/tech', (req, res) => {
+    res.render('aboutme.ejs', {
+      question: 'What excites you about technology?',
+      answer: 'tech...'
+    });
+  });
 
-app.get('/aboutme',(req,res) =>{
-  res.send ('about me info')
+  app.get('/aboutme/techstack', (req, res) => {
+    res.render('aboutme.ejs', {
+      question: 'What is your preferred technology stack?',
+      answer: 'techstack...'
+    });
+  });
+
+  app.get('/aboutme/hobbies', (req, res) => {
+    res.render('aboutme.ejs', {
+      question: 'What are your favorite hobbies?',
+      answer: 'hobbies...'
+    });
+  });
+
+  var questions = [
+    'Tell me a little bit about yourself?',
+    'What excites you about technology?',
+    'What is your preferred technology stack?',
+    'What are your favorite hobbies?'
+  ];
+
+  var answers = [
+    'description...',
+    'tech...',
+    'techstack...',
+    'hobbies...'
+  ];
+
+  res.render('aboutme.ejs', {
+    questions: questions,
+    answers: answers
+  });
 });
 
 app.get('/posts', (req,res) =>{
   //fetch the api data
   fetch('https://jsonplaceholder.typicode.com/posts')
   .then(res => res.json())
-  .then(data => { res.send({data}) })
-})
+  .then(data => {
+    res.render('posts.ejs', {
+      data: data
+    });
+  });
+});
 
 
 
@@ -42,4 +97,3 @@ app.get('/posts', (req,res) =>{
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
-
